4bd273f988a23419ceecf4c5070c950149c92576
owenx1@github.com <Owen.Byrne@student.ncirl.ie>
2013-11-23 20:40:06 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nfl::Application.config.secret_token = '5fab2059f551302f85f06bfd9cb39ca06984f4611aefe68d634c17998518970a8710f5172e75fb63c53df716b4b23bf87de54b9f96577315281391f551682667'
